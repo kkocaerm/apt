6b58b3285e0c44a4bfc99eb170cd16ff2322c37b
kkocaerm <101001082+kkocaerm@users.noreply.github.com>
2026-03-27 15:10:11 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
+
 cd /app/frontend
 npm run start -- --hostname 0.0.0.0 --port 3000 &
+
 cd /app
-uvicorn app.main:app --host 0.0.0.0 --port 10000
+exec uvicorn app.main:app --host 0.0.0.0 --port 10000
